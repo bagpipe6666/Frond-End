2133f1ab620f67f122bbe48ca3451e40577b5e60
bagpipe6666 <bumpoloyou@gmail.com>
2024-02-14 10:26:59 +0700

    update

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express  = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require ("path");
 
 // const base_url = "http://localhost:3000";
 const base_url = "http://node57860-chijom-noderest.proen.app.ruk-com.cloud";
 
 app.set('view engine', 'ejs');
+// app.set("views", path.join(__dirname,"/public/views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(__dirname + '/public/views'));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get("/", async (req,res) => {
     try { 
